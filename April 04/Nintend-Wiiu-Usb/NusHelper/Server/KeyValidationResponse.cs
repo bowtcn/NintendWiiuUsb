055fe4f589e415d756a8afed60a5fad3be4672c8
NekokoTime <38867013+NekokoTime@users.noreply.github.com>
2018-05-04 10:12:12 -0300

    Update KeyValidationResponse.cs

--- April 04/Nintend-Wiiu-Usb/NusHelper/Server/KeyValidationResponse.cs
+++ April 04/Nintend-Wiiu-Usb/NusHelper/Server/KeyValidationResponse.cs
@@ -1,6 +1,6 @@
 ﻿// Decompiled with JetBrains decompiler
 // Type: NusHelper.Server.KeyValidationResponse
-// Assembly: WiiU_USB_Helper, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// Assembly: Nintend_Wiiu_Usb, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: 53F3B763-91AD-42E2-A50A-799F56CC72D8
 // Assembly location: C:\Users\hunte\Documents\DECompiles\WiiU_USB_Helper.exe
 
